381c2c41687ca59fdc12bb0851f80ace901c01b1
Gianluca Messina <gianluca.messina8201@gmail.com>
2024-09-02 14:31:54 +0200

    Initial commit

---
+++ src/main/java/gianlucamessina/BE_U2_S1_L1/BeU2S1L1Application.java
@@ -0,0 +1,14 @@
+package gianlucamessina.BE_U2_S1_L1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeU2S1L1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeU2S1L1Application.class, args);
+
+	}
+
+}
---
+++ src/test/java/gianlucamessina/BE_U2_S1_L1/BeU2S1L1ApplicationTests.java
@@ -0,0 +1,13 @@
+package gianlucamessina.BE_U2_S1_L1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeU2S1L1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
